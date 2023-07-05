fca6a07b5a6241e3b7d20901a766dc27c33096fb
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-07-05 18:51:08 -0500

    feat: conditional sidebar render in MainLayout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,19 @@
 import Sidebar from "./Sidebar";
 import { twMerge } from "tailwind-merge";
 
-interface MainLayoutProps extends React.ComponentProps<"main"> {}
+interface MainLayoutProps extends React.ComponentProps<"main"> {
+  sidebar?: true;
+}
 
 export default function MainLayout({
   children,
   className,
+  sidebar,
   ...props
 }: React.PropsWithChildren<MainLayoutProps>) {
   return (
     <div className="flex min-h-screen bg-black">
-      <Sidebar />
+      {sidebar && <Sidebar />}
       <main
         className={twMerge(
           "border-l border-r border-gray-700 text-white",
