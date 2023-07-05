2d49aed193bbe85af3b902763e65fc09da7fe2ad
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-07-05 14:16:03 -0500

    feat: shadcn setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,44 @@
-import { type Config } from "tailwindcss";
-
-export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  colors: {
+    hover: "rgb(239,243,244,0.1)",
+    primary: "rgb(29, 155, 240)",
   },
-  plugins: [],
-} satisfies Config;
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        hover: "rgb(239,243,244,0.1)",
+        primary: "rgb(29, 155, 240)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
