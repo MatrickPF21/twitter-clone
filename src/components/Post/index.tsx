65f099b44cc684f232a5c3df9842445a5d2c84df
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-07-05 14:22:14 -0500

    feat: Post ui

---
+++ src/components/Post/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Image from "next/image";
+import PostFooter from "./Footer";
+import PostHeader from "./Header";
+
+export interface PostProps {}
+
+export default function Post(_: PostProps) {
+  return (
+    <section className="flex min-h-max gap-2 border-b border-gray-700 p-4">
+      <div className="flex-none">
+        <Image
+          className="rounded-full"
+          src="/assets/images/user.jpg"
+          alt="user image"
+          width={40}
+          height={40}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <PostHeader />
+        <div className="relative mb-1 max-h-[500px] pr-20">
+          <Image
+            src="/assets/images/post_image.jpg"
+            alt="post image"
+            fill={true}
+            className="!relative !w-auto rounded-3xl border border-gray-700"
+          />
+        </div>
+        <PostFooter />
+      </div>
+    </section>
+  );
+}
