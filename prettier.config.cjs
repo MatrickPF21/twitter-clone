e06c7f6a12ea3b96dfae39be3b982292645ea56a
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-07-22 16:19:08 -0500

    feat: prettier setup

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
+  useTabs: false,
+  jsxSingleQuote: true,
+  arrowParens: "avoid",
+  semi: true,
+  tabWidth: 2,
 };
 
 module.exports = config;
