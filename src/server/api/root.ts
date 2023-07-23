b117e69680f8f46a5269d9175b9a7222c9ca603a
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-07-22 20:16:07 -0500

    fix: removed example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { exampleRouter } from "~/server/api/routers/example";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { profileRouter } from "./routers/profile";
 import { tweetsRouter } from "./routers/tweets";
@@ -9,7 +8,6 @@ import { tweetsRouter } from "./routers/tweets";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   profile: profileRouter,
   tweet: tweetsRouter,
 });
