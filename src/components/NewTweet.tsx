44fb2878670ec5a4d2e85682f3feb9e5eddb78b3
MatrickPF21 <patrickfigueroa23@gmail.com>
2023-09-20 18:04:43 -0500

    feat(NewTweet): trim input

--- src/components/NewTweet.tsx
+++ src/components/NewTweet.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { useAutosizeTextArea } from "~/hooks/useAutosizeTextArea";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
-import { Button } from "./ui/button";
+import Link from "next/link";
+import React from "react";
+
+import { useAutosizeTextArea } from "~/hooks/useAutosizeTextArea";
 import { api } from "~/utils/api";
+import { Button } from "./ui/button";
 
 export default function NewTweet() {
+  const { data } = useSession();
   const context = api.useContext();
   const [value, setValue] = React.useState("");
   const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
@@ -25,6 +29,8 @@ export default function NewTweet() {
   const handleSubmit = async (evt: React.FormEvent<HTMLFormElement>) => {
     evt.preventDefault();
 
+    if (!value.trim()) return;
+
     mutate({ text: value });
   };
 
@@ -33,15 +39,15 @@ export default function NewTweet() {
       <form onSubmit={handleSubmit}>
         <div className='flex flex-col gap-2 p-2 pt-4'>
           <div className='flex gap-4'>
-            <div>
+            <Link href={`/users/${data?.user.id}`}>
               <Image
                 className='aspect-square rounded-full'
-                src='/assets/images/user.jpg'
-                alt='user image'
+                src={data?.user.image || "/assets/images/user.jpg"}
+                alt={`${data?.user.name} user image`}
                 width={40}
                 height={40}
               />
-            </div>
+            </Link>
             <textarea
               className='w-full bg-transparent text-xl outline-none'
               placeholder="What's happening?"
@@ -50,10 +56,11 @@ export default function NewTweet() {
               rows={1}
               value={value}
               disabled={isLoading}
+              required
             />
           </div>
           <div className='flex justify-end p-2'>
-            <Button disabled={isLoading}>Tweet</Button>
+            <Button disabled={isLoading || !data?.user}>Tweet</Button>
           </div>
         </div>
       </form>
